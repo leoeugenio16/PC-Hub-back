fe5076023510d0f67c832e7aefd35b4d858af2bf
leoeugenio16 <leoeugenio16@gmail.com>
2024-11-06 13:43:59 -0300

    update

--- config/server.ts
+++ config/server.ts
@@ -1,6 +1,6 @@
-export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+module.exports = ({ env }) => ({
+  host: '0.0.0.0', // Aseguramos que esté escuchando en todas las interfaces.
+  port: process.env.PORT || 1337, // Usamos el puerto de Render (también se puede forzar si lo necesitas)
   app: {
     keys: env.array('APP_KEYS'),
   },
