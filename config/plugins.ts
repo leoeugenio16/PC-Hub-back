a6d87f8eb553a6ff396769abe7e6e22e6b4a2c6b
Leandro Eugenio <leoeugenio16@gmail.com>
2024-11-09 20:29:07 -0300

    update con imagenes en cludinary

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,11 @@
-export default () => ({});
+module.exports = {
+    upload: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: 'djm8mgfsr',  // Tu Cloud Name
+        api_key: '345458947453629',  // Tu API Key
+        api_secret: '190y4XswYmwtVDJpWZqJRO84Uew',  // Tu API Secret
+      },
+    },
+  };
+  
